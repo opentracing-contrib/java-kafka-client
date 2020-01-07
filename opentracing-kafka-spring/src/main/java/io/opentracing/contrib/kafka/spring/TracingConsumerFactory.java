bcbb0cce75832ff4f2bd59984eee2a3d4c00f3c8
Michele <lars.michele@tu-dortmund.de>
2020-01-07 21:59:19 +0100

    Adding the possibility to have custom span tags for producing and reading also for Kafka Spring and Kafka Streams.

--- opentracing-kafka-spring/src/main/java/io/opentracing/contrib/kafka/spring/TracingConsumerFactory.java
+++ opentracing-kafka-spring/src/main/java/io/opentracing/contrib/kafka/spring/TracingConsumerFactory.java
@@ -13,9 +13,14 @@
  */
 package io.opentracing.contrib.kafka.spring;
 
+import static io.opentracing.contrib.kafka.SpanDecorator.STANDARD_TAGS;
+
 import io.opentracing.Tracer;
 import io.opentracing.contrib.kafka.ClientSpanNameProvider;
-import io.opentracing.contrib.kafka.TracingKafkaConsumer;
+import io.opentracing.contrib.kafka.SpanDecorator;
+import io.opentracing.contrib.kafka.TracingKafkaConsumerBuilder;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Properties;
 import java.util.function.BiFunction;
@@ -28,18 +33,30 @@ public class TracingConsumerFactory<K, V> implements ConsumerFactory<K, V> {
 
   private final ConsumerFactory<K, V> consumerFactory;
   private final Tracer tracer;
+  private final Collection<SpanDecorator> spanDecorators;
   private final BiFunction<String, ConsumerRecord, String> consumerSpanNameProvider;
 
   public TracingConsumerFactory(ConsumerFactory<K, V> consumerFactory, Tracer tracer) {
-    this.tracer = tracer;
-    this.consumerFactory = consumerFactory;
-    this.consumerSpanNameProvider = ClientSpanNameProvider.CONSUMER_OPERATION_NAME;
+    this(consumerFactory, tracer, null, null);
+  }
+
+  public TracingConsumerFactory(ConsumerFactory<K, V> consumerFactory, Tracer tracer,
+      Collection<SpanDecorator> spanDecorators) {
+    this(consumerFactory, tracer, spanDecorators, null);
   }
 
   public TracingConsumerFactory(ConsumerFactory<K, V> consumerFactory, Tracer tracer,
       BiFunction<String, ConsumerRecord, String> consumerSpanNameProvider) {
+    this(consumerFactory, tracer, null, consumerSpanNameProvider);
+  }
+
+  public TracingConsumerFactory(ConsumerFactory<K, V> consumerFactory, Tracer tracer,
+      Collection<SpanDecorator> spanDecorators, BiFunction<String, ConsumerRecord, String> consumerSpanNameProvider) {
     this.tracer = tracer;
     this.consumerFactory = consumerFactory;
+    this.spanDecorators = (spanDecorators == null)
+        ? Collections.singletonList(STANDARD_TAGS)
+        : spanDecorators;
     this.consumerSpanNameProvider = (consumerSpanNameProvider == null)
         ? ClientSpanNameProvider.CONSUMER_OPERATION_NAME
         : consumerSpanNameProvider;
@@ -47,35 +64,34 @@ public class TracingConsumerFactory<K, V> implements ConsumerFactory<K, V> {
 
   @Override
   public Consumer<K, V> createConsumer() {
-    return new TracingKafkaConsumer<>(consumerFactory.createConsumer(), tracer,
-        consumerSpanNameProvider);
+    return new TracingKafkaConsumerBuilder<>(consumerFactory.createConsumer(), tracer)
+        .withDecorators(spanDecorators).withSpanNameProvider(consumerSpanNameProvider).build();
   }
 
   @Override
   public Consumer<K, V> createConsumer(String clientIdSuffix) {
-    return new TracingKafkaConsumer<>(consumerFactory.createConsumer(clientIdSuffix), tracer,
-        consumerSpanNameProvider);
+    return new TracingKafkaConsumerBuilder<>(consumerFactory.createConsumer(clientIdSuffix), tracer)
+        .withDecorators(spanDecorators).withSpanNameProvider(consumerSpanNameProvider).build();
   }
 
   @Override
   public Consumer<K, V> createConsumer(String groupId, String clientIdSuffix) {
-    return new TracingKafkaConsumer<>(consumerFactory.createConsumer(groupId, clientIdSuffix),
-        tracer, consumerSpanNameProvider);
+    return new TracingKafkaConsumerBuilder<>(consumerFactory.createConsumer(groupId, clientIdSuffix), tracer)
+        .withDecorators(spanDecorators).withSpanNameProvider(consumerSpanNameProvider).build();
   }
 
   @Override
-  public Consumer<K, V> createConsumer(String groupId, String clientIdPrefix,
-      String clientIdSuffix) {
-    return new TracingKafkaConsumer<>(
-        consumerFactory.createConsumer(groupId, clientIdPrefix, clientIdSuffix),
-        tracer, consumerSpanNameProvider);
+  public Consumer<K, V> createConsumer(String groupId, String clientIdPrefix, String clientIdSuffix) {
+    return new TracingKafkaConsumerBuilder<>(consumerFactory.createConsumer(groupId, clientIdPrefix, clientIdSuffix),
+        tracer).withDecorators(spanDecorators).withSpanNameProvider(consumerSpanNameProvider).build();
   }
 
   @Override
   public Consumer<K, V> createConsumer(String groupId, String clientIdPrefix,
       String clientIdSuffix, Properties properties) {
-    return new TracingKafkaConsumer<>(consumerFactory.createConsumer(groupId, clientIdPrefix,
-        clientIdSuffix, properties), tracer, consumerSpanNameProvider);
+    return new TracingKafkaConsumerBuilder<>(consumerFactory.createConsumer(groupId, clientIdPrefix,
+        clientIdSuffix, properties), tracer).withDecorators(spanDecorators)
+        .withSpanNameProvider(consumerSpanNameProvider).build();
   }
 
   @Override
--- opentracing-kafka-streams/src/main/java/io/opentracing/contrib/kafka/streams/TracingKafkaClientSupplier.java
+++ opentracing-kafka-streams/src/main/java/io/opentracing/contrib/kafka/streams/TracingKafkaClientSupplier.java
@@ -13,11 +13,16 @@
  */
 package io.opentracing.contrib.kafka.streams;
 
+import static io.opentracing.contrib.kafka.SpanDecorator.STANDARD_TAGS;
+
 import io.opentracing.Tracer;
 import io.opentracing.contrib.kafka.ClientSpanNameProvider;
-import io.opentracing.contrib.kafka.TracingKafkaConsumer;
-import io.opentracing.contrib.kafka.TracingKafkaProducer;
+import io.opentracing.contrib.kafka.SpanDecorator;
+import io.opentracing.contrib.kafka.TracingKafkaConsumerBuilder;
+import io.opentracing.contrib.kafka.TracingKafkaProducerBuilder;
 import io.opentracing.util.GlobalTracer;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 import java.util.function.BiFunction;
 import org.apache.kafka.clients.admin.AdminClient;
@@ -34,26 +39,39 @@ import org.apache.kafka.streams.KafkaClientSupplier;
 public class TracingKafkaClientSupplier implements KafkaClientSupplier {
 
   private final Tracer tracer;
+  private final Collection<SpanDecorator> spanDecorators;
   private final BiFunction<String, ConsumerRecord, String> consumerSpanNameProvider;
   private final BiFunction<String, ProducerRecord, String> producerSpanNameProvider;
 
   public TracingKafkaClientSupplier(Tracer tracer) {
-    this.tracer = tracer;
-    this.consumerSpanNameProvider = ClientSpanNameProvider.CONSUMER_OPERATION_NAME;
-    this.producerSpanNameProvider = ClientSpanNameProvider.PRODUCER_OPERATION_NAME;
+    this(tracer, null, null, null);
   }
 
   /**
    * GlobalTracer is used to get tracer
    */
   public TracingKafkaClientSupplier() {
-    this(GlobalTracer.get());
+    this(GlobalTracer.get(), null, null, null);
+  }
+
+  public TracingKafkaClientSupplier(Tracer tracer,Collection<SpanDecorator> spanDecorators) {
+    this(tracer, spanDecorators,null, null);
   }
 
   public TracingKafkaClientSupplier(Tracer tracer,
       BiFunction<String, ConsumerRecord, String> consumerSpanNameProvider,
       BiFunction<String, ProducerRecord, String> producerSpanNameProvider) {
+    this(tracer, null, consumerSpanNameProvider, producerSpanNameProvider);
+  }
+
+  public TracingKafkaClientSupplier(Tracer tracer,
+      Collection<SpanDecorator> spanDecorators,
+      BiFunction<String, ConsumerRecord, String> consumerSpanNameProvider,
+      BiFunction<String, ProducerRecord, String> producerSpanNameProvider) {
     this.tracer = tracer;
+    this.spanDecorators = (spanDecorators == null)
+        ? Collections.singletonList(STANDARD_TAGS)
+        : spanDecorators;
     this.consumerSpanNameProvider = (consumerSpanNameProvider == null)
         ? ClientSpanNameProvider.CONSUMER_OPERATION_NAME
         : consumerSpanNameProvider;
@@ -68,7 +86,7 @@ public class TracingKafkaClientSupplier implements KafkaClientSupplier {
   public TracingKafkaClientSupplier(
       BiFunction<String, ConsumerRecord, String> consumerSpanNameProvider,
       BiFunction<String, ProducerRecord, String> producerSpanNameProvider) {
-    this(GlobalTracer.get(), consumerSpanNameProvider, producerSpanNameProvider);
+    this(GlobalTracer.get(), null, consumerSpanNameProvider, producerSpanNameProvider);
   }
 
   // This method is required by Kafka Streams >=1.1, and optional for Kafka Streams <1.1
@@ -79,29 +97,29 @@ public class TracingKafkaClientSupplier implements KafkaClientSupplier {
 
   @Override
   public Producer<byte[], byte[]> getProducer(Map<String, Object> config) {
-    return new TracingKafkaProducer<>(
+    return new TracingKafkaProducerBuilder<>(
         new KafkaProducer<>(config, new ByteArraySerializer(), new ByteArraySerializer()),
-        tracer, producerSpanNameProvider);
+        tracer).withDecorators(spanDecorators).withSpanNameProvider(producerSpanNameProvider).build();
   }
 
   @Override
   public Consumer<byte[], byte[]> getConsumer(Map<String, Object> config) {
-    return new TracingKafkaConsumer<>(
+    return new TracingKafkaConsumerBuilder<>(
         new KafkaConsumer<>(config, new ByteArrayDeserializer(), new ByteArrayDeserializer()),
-        tracer, consumerSpanNameProvider);
+        tracer).withDecorators(spanDecorators).withSpanNameProvider(consumerSpanNameProvider).build();
   }
 
   @Override
   public Consumer<byte[], byte[]> getRestoreConsumer(Map<String, Object> config) {
-    return new TracingKafkaConsumer<>(
+    return new TracingKafkaConsumerBuilder<>(
         new KafkaConsumer<>(config, new ByteArrayDeserializer(), new ByteArrayDeserializer()),
-        tracer, consumerSpanNameProvider);
+        tracer).withDecorators(spanDecorators).withSpanNameProvider(consumerSpanNameProvider).build();
   }
 
   @Override
   public Consumer<byte[], byte[]> getGlobalConsumer(Map<String, Object> config) {
-    return new TracingKafkaConsumer<>(
+    return new TracingKafkaConsumerBuilder<>(
         new KafkaConsumer<>(config, new ByteArrayDeserializer(), new ByteArrayDeserializer()),
-        tracer, consumerSpanNameProvider);
+        tracer).withDecorators(spanDecorators).withSpanNameProvider(consumerSpanNameProvider).build();
   }
 }
